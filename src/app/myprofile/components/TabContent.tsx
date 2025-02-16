acbf68fee457aea146c81486f810273a2651d1e9
Charie95 <lus0905@gmail.com>
2025-02-17 02:04:09 +0900

    리뷰/등록와인  탭 분리 및 컴포넌트 제작

--- src/app/myprofile/components/TabContent.tsx
+++ src/app/myprofile/components/TabContent.tsx
@@ -1,19 +1,21 @@
 "use client";
 
+import MyReviews from "./MyReivews"; // 내가 쓴 후기 컴포넌트
+
 interface TabContentProps {
   activeTab: number;
 }
 
 export default function TabContent({ activeTab }: TabContentProps) {
   return (
-    <div>
+    <div className="w-full">
       {activeTab === 1 ? (
-        <div>
-          <h2>내가 쓴 후기</h2>
-        </div>
+        <MyReviews /> // ✅ 내가 쓴 후기 불러오기
       ) : activeTab === 2 ? (
         <div>
-          <h2>내가 등록한 와인</h2>
+          {" "}
+          {/* 내가 등록한 와인은 아직 미구현 상태 */}
+          <h2>내가 등록한 와인 (구현 예정)</h2>
         </div>
       ) : null}
     </div>
