ebc80e78270a9d96d54bf600d6257deec49b07a1
Namgyeon <getam101@naver.com>
2025-02-16 23:46:27 +0900

    Style: Toast 주석 추가

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -4,6 +4,17 @@ import clsx from "clsx";
 
 type ToastType = "success" | "error" | "info";
 
+/**
+ * 추가 하고싶은 내용이 있으면 추가해서 사용하시면 됩니다.
+ * 사용법:
+ * 1.사용하고자 하는 페이지 layout에 winelist/[id]/layout.tsx파일에 있는 것처럼
+ *   토스트를 import하고 <Toaster />컴포넌트를 넣어주면 됩니다.
+ *
+ * 2. import해서 메시지와 타입을 매개변수로 넣어주면 끝
+ * showToast("리뷰가 성공적으로 등록되었습니다.", "success");
+ *
+ */
+
 export const showToast = (message: string, type: ToastType = "info") => {
   sonnerToast.custom(() => (
     <div
